39fc464148a1d254b5f6b1e3174c32429bf4298d
Chethan Y <chethan1n1@users.noreply.github.com>
2026-04-04 17:32:34 +0530

    frontend ready for vercel

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://bucketing-tool.onrender.com";
 const STORAGE_THEME_KEY = "bucketing-theme";
 
 const form = document.getElementById("predict-form");
